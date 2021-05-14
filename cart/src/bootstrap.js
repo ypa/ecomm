d01a55fd929a7e2fdedb316304db302a6502d346
Yan P Aung <yan.pye.aung@gmail.com>
2021-05-13 20:41:39 -0700

    Refactor cart app to be able to run in both local developent context as well as via container/prod context. And then also update to use mount in container app.

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,29 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+  const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#cart-dev').innerHTML = cartText;
+  el.innerHTML = cartText;
+};
+
+// Context/Situation #1
+// Running in development in isolation
+// We are using our local index.html file
+// We definitely has an element with an id of 'cart-dev'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#cart-dev');
+
+  // Assuming our container doesn't have an element
+  // with id 'dev-products'.
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
+}
+
+// Context/Situation #2
+// Running in development or production through a container app
+// No gurantee that an element with an specific id exists
+// We don't want to immediately render the app
+export { mount };
