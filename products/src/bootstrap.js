64a439776a007f953defb55eb0c67928c715d2c8
Yan P Aung <yan.pye.aung@gmail.com>
2021-05-13 20:27:00 -0700

    Refactor products app to be able to run in both local developent context as well as via container/prod context

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,34 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// Running in development in isolation
+// We are using our local index.html file
+// We definitely has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+
+  // Assuming our container doesn't have an element
+  // with id 'dev-products'.
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// Context/Situation #2
+// Running in development or production through a container app
+// No gurantee that an element with an specific id exists
+// We don't want to immediately render the app
+export { mount };
